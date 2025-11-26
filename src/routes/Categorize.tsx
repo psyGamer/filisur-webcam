37dbdf1e732b738ca1c9df57f02362027dc927f3
psyGamer <psygamer21@gmail.com>
2025-11-26 14:30:48 +0100

    Add player for previewing uncategorized videos

--- src/routes/Categorize.tsx
+++ src/routes/Categorize.tsx
@@ -1,8 +1,33 @@
-import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
+import { useEffect, useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query"
 import { useSearchParams } from "react-router-dom"
+
+import ReactPlayer from "react-player";
+import {
+    MediaController,
+    MediaControlBar,
+    MediaTimeRange,
+    MediaTimeDisplay,
+    MediaVolumeRange,
+    MediaPlaybackRateButton,
+    MediaPlayButton,
+    MediaSeekBackwardButton,
+    MediaSeekForwardButton,
+    MediaMuteButton,
+    MediaFullscreenButton,
+    MediaLoadingIndicator,
+} from "media-chrome/react";
+import { 
+    MediaPlaybackRateMenu as MediaPlaybackRateMenuType, 
+    MediaPlaybackRateMenuButton as MediaPlaybackRateMenuButtonType,
+} from "media-chrome/menu";
+import { 
+    MediaPlaybackRateMenu, 
+    MediaPlaybackRateMenuButton 
+} from "media-chrome/react/menu";
+
 import VerticalVideoSelector from "../components/VerticalVideoSelector"
-import VideoButton from "../components/VideoButton"
 
 import './Categorize.scss'
 
@@ -15,6 +40,47 @@ const useFetchPending = () => useQuery({
 function Categorize() {
     const [searchParams, setSearchParams] = useSearchParams()
     const { data, error, isFetching, isLoading } = useFetchPending()
+
+    const [selectedVideo, setSelectedVideo] = useState<number>(0)
+
+    const playbackMenuRef = useRef<MediaPlaybackRateMenuType>(null)
+    const playbackButtonRef = useRef<MediaPlaybackRateMenuButtonType>(null)
+
+    useEffect(() => {
+        if (!playbackMenuRef.current || !playbackButtonRef.current) return
+        console.log(playbackMenuRef.current, playbackButtonRef.current)
+
+        // Load preference
+        const preferredPlaybackSpeed = localStorage.getItem("categorize-playback-speed")
+        if (preferredPlaybackSpeed) {
+            const rate = JSON.parse(preferredPlaybackSpeed)
+            playbackMenuRef.current.mediaPlaybackRate = rate
+            playbackButtonRef.current.mediaPlaybackRate = rate
+        }
+
+        let firstMutation = preferredPlaybackSpeed != null
+
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                // Hack since it tries to change it back to 1x on it's own..
+                if (firstMutation) {
+                    const rate = JSON.parse(preferredPlaybackSpeed!)
+                    playbackMenuRef.current!.mediaPlaybackRate = rate
+                    playbackButtonRef.current!.mediaPlaybackRate = rate
+                    firstMutation = false
+                }
+
+                // Save preference
+                if (mutation.type == "attributes" && playbackMenuRef.current) {
+                    console.log(`Changed ${playbackMenuRef.current.mediaPlaybackRate}`)
+                    localStorage.setItem("categorize-playback-speed", playbackMenuRef.current.mediaPlaybackRate.toString())
+                }
+            });
+        })
+        observer.observe(playbackMenuRef.current, { attributes: true })
+
+        return () => observer.disconnect()
+    })
 
     if (isFetching || isLoading) {
         return <p>Loading...</p>
@@ -30,17 +96,61 @@ function Categorize() {
         const url = new URL(window.location.href);
         url.searchParams.set("video", videos[0]);
         window.history.pushState({}, "", url);
+
+        setSelectedVideo(selectedIdx)
     }
 
     return <>
-        <VerticalVideoSelector sources={videos} selectedIndex={selectedIdx >= 0 ? selectedIdx : null} onSelectedChanged={idx => {
-            const url = new URL(window.location.href);
-            url.searchParams.set("video", videos[idx]);
-            window.history.pushState({}, "", url);
+        <div
+            style={{
+                width: '100vw',
+                height: '100vh',
+                display: 'flex',
+            }}
+        >
+            <VerticalVideoSelector sources={videos} selectedIndex={selectedIdx >= 0 ? selectedIdx : null} onSelectedChanged={idx => {
+                const url = new URL(window.location.href)
+                url.searchParams.set("video", videos[idx])
+                window.history.pushState({}, "", url)
 
-            // setSearchParams({ video: ":3" })
-            // console.log(searchParams.get("video"))
-        }} />
+                setSelectedVideo(idx)
+            }} />
+
+            <MediaController
+                style={{
+                    width: '70rem',
+                    height: 'fit-content',
+                    margin: '1.5rem 0',
+                    borderRadius: 'var(--border-radius)'
+                }}
+            >
+                <ReactPlayer 
+                    slot="media"
+                    src={`/cdn/video/${videos[selectedVideo]}`}
+                    playing={true}
+                    style={{
+                        width: '100%',
+                        height: 'auto',
+                        aspectRatio: '11/9'
+                    }} />
+                
+                <MediaLoadingIndicator />
+                <MediaPlaybackRateMenu 
+                    ref={playbackMenuRef}
+                    rates={[1,2,3,4,5,10,15]} 
+                    mediaPlaybackRate={3}
+                    id='playback-menu'
+                    hidden />
+                <MediaControlBar>
+                    <MediaPlayButton />
+                    <MediaSeekBackwardButton seekOffset={15} />
+                    <MediaSeekForwardButton seekOffset={15} />
+                    <MediaTimeRange />
+                    <MediaTimeDisplay showDuration />
+                    <MediaPlaybackRateMenuButton invokeTarget='playback-menu' ref={playbackButtonRef} />
+                </MediaControlBar>
+            </MediaController>
+        </div>
     </>
 }
 
